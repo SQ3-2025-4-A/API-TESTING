c53e7ac4232dd46c7ca101eecd9ac03dc0c0ab7e
rubiperucci <rubi_perucci@hotmail.com>
2025-05-10 01:05:52 -0300

    Add Newman HTML Reporter

---
+++ newman/newman_reporter.js
@@ -0,0 +1,26 @@
+const newman = require('newman');
+ 
+const collections = { 
+    'wrike': require('./WRIKE.postman_collection.json'),
+};
+
+for (let i of Object.keys(collections)){
+    newman.run({
+        collection: collections[i],
+        reporters: 'html',
+        reporter: {
+            html: {
+                export: `reports/${i}.html`
+            }
+        }
+    }).on('start', function (err, args) { 
+        console.log('running a collection...');
+    }).on('done', function (err, summary) {
+        if (err || summary.error) {
+            console.error('collection run encountered an error.');
+        }
+        else {
+            console.log('collection run completed.');
+        }
+    })
+};
