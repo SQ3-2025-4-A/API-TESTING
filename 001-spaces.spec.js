5e351bec00fbb17efaf2bf933deaa8182f134b13
rubiperucci <rubi_perucci@hotmail.com>
2025-05-13 19:48:39 -0300

    Add schema validation for  response

--- 001-spaces.spec.js
+++ 001-spaces.spec.js
@@ -5,6 +5,22 @@ const requestManager = require("./requestManager");
 describe("Wrike API Tests", () => {
     describe("Spaces", () => {
         test("Verify that is possible to retrieve a list of spaces", async () => {
+            const schema = joi.object({
+                kind: joi.string().valid("spaces").required(),
+                data: joi.array().items(
+                    joi.object({
+                        id: joi.string().required(),
+                        title: joi.string().required(),
+                        avatarUrl: joi.string().uri().required(),
+                        accessType: joi.string().valid("Public", "Personal").required(),
+                        archived: joi.boolean().required(),
+                        guestRoleId: joi.string(), //opcional
+                        defaultProjectWorkflowId: joi.string(),
+                        defaultTaskWorkflowId: joi.string() 
+                    })
+                ).required()
+            });
+
             let response = await requestManager.send(
                 "GET", 
                 "spaces", 
@@ -14,9 +30,12 @@ describe("Wrike API Tests", () => {
                 }
             );
 
-            expect(response.status).toBe(201);
+            let validationResult = schema.validate(response.data);
+
+            expect(response.status).toBe(200);
             expect(response.data).toHaveProperty("data");
             expect(response.data.data).toBeInstanceOf(Array);
+            expect(validationResult.error).toBeUndefined();
         })
     })
 });
