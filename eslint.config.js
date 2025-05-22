a211ccc03b76508e1294b1d720c3066d6a856a81
rubiperucci <rubi_perucci@hotmail.com>
2025-05-22 20:43:37 -0300

    Add linter

---
+++ eslint.config.js
@@ -0,0 +1,31 @@
+import { defineConfig } from "eslint/config";
+import js from "@eslint/js";
+
+export default defineConfig([
+  {    
+    ignores: [
+      "node_modules/*",
+      "reports/*",
+      "**/newman/*"
+    ],
+  },
+  { 
+    files: [
+      "**/*.js"
+    ], 
+    plugins: {
+      js 
+    }, 
+    extends: [
+      "js/recommended"
+    ]
+  },
+	{
+		rules: {
+			"no-unused-vars": "error",
+      "no-undef": "off",
+      quotes: ["error", "double"],
+			semi: ["error", "always"],
+		},
+	},
+]);
